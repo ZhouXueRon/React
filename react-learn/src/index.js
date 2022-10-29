3d4351ca0a28f3a7517bffcdcef7a16ce8480431
zxr <1809178201@qq.com>
2022-10-30 00:55:49 +0800

    组件状态

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StudentList from './components/StudentList';
+import Tick from './components/Tick';
 
-const appkey = "demo13_1545210570249";
-/**
- * 获取所以的学生数据
- */
-const fetchAllStudents = async() => {
-    const stus = await fetch("http://api.duyiedu.com/api/student/findAll?appkey=" + appkey)
-        .then(resp => resp.json()).then(resp => resp.data);
-    return stus;
-}
+ReactDOM.render(<Tick number={10} />, document.getElementById('root'));
 
-const render = async() => {
-    ReactDOM.render('正在加载中......', document.getElementById('root'));
-    const stus = await fetchAllStudents(); // 获取学生数组
-    ReactDOM.render(<StudentList stus={stus} />, document.getElementById('root'));
-}
 
-render();
+
 
 
 
