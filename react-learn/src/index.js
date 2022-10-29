ffed4cd41d2e674e3dfb25f1569f863494161991
zxr <1809178201@qq.com>
2022-10-30 00:58:25 +0800

    master修改

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Tick from './components/Tick';
 
-ReactDOM.render(<Tick number={10} />, document.getElementById('root'));
+ReactDOM.render(<div></div>, document.getElementById('root'));
 
 
 
