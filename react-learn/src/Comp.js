c330bb81d3eba3d383f65e8edeea41ca833f138e
zxr <1809178201@qq.com>
2022-11-01 21:10:01 +0800

    传递元素内容

---
+++ react-learn/src/Comp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Comp(props) {
+    return (
+        <div className='comp'>
+            {props.children}
+        </div>
+    )
+}
--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PagerTest from './components/PagerTest';
+import Comp from './Comp';
 
-ReactDOM.render(<PagerTest />, document.getElementById('root'));
+ReactDOM.render(
+    <Comp>
+        <h1>这是我传递过来的内容</h1>
+    </Comp>
+, document.getElementById('root'));
