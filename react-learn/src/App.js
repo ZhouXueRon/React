b8bc161c14854091a4a23f5909272e6439e5eb90
zxr <1809178201@qq.com>
2022-11-02 21:37:25 +0800

    修改master

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Test from './components/common/Select/Test';
 
 export default function App() {
     return (
-        <div>
-            <Test />
-        </div>
+        <div></div>
     )
 }
